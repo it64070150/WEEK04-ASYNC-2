0d5df2370415cf6f874afd83a71a73babc6a6204
it64070150 <88415632+it64070150@users.noreply.github.com>
2023-02-10 17:44:52 +0700

    Add files via upload

--- exercise-03/answer.js
+++ exercise-03/answer.js
@@ -1,8 +1,19 @@
 // ข้อ 3.1
 function getDogDemo(url) {
   // hint: เรียกใช้ getAPI() โดยดึงข้อมูลจาก url = https://dog.ceo/api/breeds/image/random
-  // ลอง console.log() ดูว่าข้อมูลที่ได้มาเป็นอย่างไร
-
+  // ลอง console.log() ดูว่าข้อมูลที่ได้มาเป็นอย่างไรurl = "https://dog.ceo/api/breeds/image/random";
+  url = "https://dog.ceo/api/breeds/image/random";
+  getAPI(url,success,error)
+    function success(img){
+      console.log(img)
+      setTimeout(()=>{
+        document.getElementById("dogImg").src = img.message;
+      },10000)
+    
+  }
+  function error(error){
+    console.log(error)
+  } 
 }
 
 
@@ -14,11 +25,40 @@ function Rainbow() {
   // 3. เปลี่ยนสีข้อความเป็นสีแดงเมื่อเกิด Error (class = 'has-text-error')
 
   const rainbow = document.getElementById("rainbow")
+
   setTimeout(() => {
+    var check = getResult();
+  if (check.status == 'success') {
+      rainbow.innerHTML = check.text;
+      rainbow.classList.add('has-text-primary');
+  }else{
+    rainbow.innerHTML = "STATE-1";
+    rainbow.classList.add('has-text-danger');
+    
+  }
     // STATE 1 color = 'has-text-primary'
-      setTimeout(() => {
+  setTimeout(() => {
+     var check1 = getResult();
+  if (check1.status == 'success') {
+      rainbow.innerHTML = check1.text;
+      rainbow.classList.add('has-text-success');
+  }else{
+    rainbow.innerHTML = "STATE-2";
+    rainbow.classList.add('has-text-danger');
+    
+  }
+        
         // STATE 2 color = 'has-text-success'
         setTimeout(() => {
+      var check2 = getResult();
+  if (check2.status == 'success') {
+      rainbow.innerHTML = check1.text;
+      rainbow.classList.add('has-text-success');
+  }else{
+    rainbow.innerHTML = "STATE-3";
+    rainbow.classList.add('has-text-danger');
+    
+  }
             // STATE 3 color = 'has-text-success'
         }, 2000)
 
@@ -41,33 +81,99 @@ function getResult(){
       'text': num
     }
   }
+  
+}
+
+const even = async() => {
+  if (num % 2 === 0) {
+    //
+  }
+  else {
+    //
+  }
 }
 
 // ข้อ 3.3
-function evenNumber(num) {
+async function evenNumber(num) {
   // hint : ทำการสร้าง promise และเรียกใช้
-}
+   // The producing code (this may take some time)
+  // let result = new Promise(function(myResolve, myReject) {
+  //   if (num % 2 == 0) {
+  //     myResolve ("success : "+ num +" is an even number");
+  //   } else {
+  //     myReject ( "Error : "+ num +" is not an even number");
+  //   }
+  // });
+  // result.then((value) => {
+  //   document.getElementById("result").innerHTML = value; // Success!
+  // }).catch((error) => {
+  //   document.getElementById("result").innerHTML = error; // Error!
+  // })
 
+ }
+ 
 // ข้อ 3.4
 function task(id) {
   const delay = parseInt(Math.random() * 1000)
   // return promise
+  return new Promise(function(myResolve, myReject) {
+    setTimeout(() => {
+      if (delay < 500) {
+        myResolve("task : "+ id + delay +"ms ... PASS!");
+      } else {
+        myReject("task : "+ id + delay +"ms ... NOTPASS!");
+      }
+    }, delay)
+    });
 }
 
 function tester() {
   // hint : task(1).then().catch() ..... task(4)...
   // ต้องเรียก function task 4 ครั้ง เปลี่ยน id ไปเรื่อยๆ
+  task(1).then((res) =>{
+    console.log(res)
+  }).catch((err) =>{
+    console.log(err)
+  })
+  task(2).then((res) =>{
+    console.log(res)
+  }).catch((err) =>{
+    console.log(err)
+  })
+  task(3).then((res) =>{
+    console.log(res)
+  }).catch((err) =>{
+    console.log(err)
+  })
+  task(4).then((res) =>{
+    console.log(res)
+  }).catch((err) =>{
+    console.log(err)
+  })
 }
 
 // ข้อ 3.5
 // hint : เรียก getAPI() ที่ url = https://api.thecatapi.com/v1/images/search 
 // อย่าลืม console.log() ดูข้อมูลที่ได้ด้วยว่ามีโครงสร้างแบบใด
 function checkAuth(password) {
-  
-}
+  return new Promise((resolve, reject) => {
+    if(password == "Cisco"){
+      resolve("รหัสผ่านถูกต้อง")
+     }
+    else{
+      reject("รหัสผ่านไม่ถูกต้อง กรุณาลองอีกครั้ง")
+     }
+ })
+  }
 
 function fetchData(password) {
-  
+  checkAuth(password).then((res) => {
+    alert(res)
+    getAPI("https://api.thecatapi.com/v1/images/search", (res) =>
+     document.getElementById("cat").src = res[0].url)
+  }).catch((err)=>
+     alert(err)
+  )
 }
 
 // GET API
--- exercise-04/answer.js
+++ exercise-04/answer.js
@@ -1,22 +1,38 @@
 // ข้อ4.1
-function getAllUser(){
+async function getAllUser(){
     //TODO
     // 1. ให้ใช้ Try Catch
     // 2. เรียกใช้ฟังก์ชัน ApiDelay()
     // 3. หากเรียกฟังก์ชันสำเร็จให้ (status: 200) ให้นำ message แสดงในกล่องข้อความ
     // 4. หากเรียกฟังก์ชันไม่สำเร็จ (message: "SERVER ERROR") ให้นำ message แสดงในกล่องข้อความ
-
+    let result = document.getElementById("TA")
+    try {
+        const res = await ApiDelay()
+        console.log(res);
+        result.innerText = res.message
+    }catch (error){
+        result.innerText = error.message
+    }
 }
 
 // ข้อ 4.2 
 function checkAuth(password) {
+    if (password === "In4matioN"){
+        return "รหัสผ่านถูกต้อง"
+    } else {
+        throw new Error("รหัสผ่านไม่ถูกต้อง ลองใหม่")
+    }
 
 }
 
 async function fetchData(password) {
    try {
-       
+    alert(checkAuth(password))
+    const res = await fetch("https://api.thecatapi.com/v1/images/search")
+    const data = await res.json()
+    document.getElementById("cat").src = data[0].url 
    } catch (error) {
+    alert(error.message);
        
    }
 }
